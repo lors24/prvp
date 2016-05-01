5077bb077fba2c2570eecaea00ba874249c928ef
Lore <lors2404@gmail.com>
2016-05-01 00:05:31 -0500

    finaliza lectura de info

--- leer_datos.m
+++ leer_datos.m
@@ -35,16 +35,50 @@ qs = M(ini+1:end,5);
 fs = M(ini+1:end,6);
 %num_combs = M(ini+1:end,7);
 
-datos = {m,n,p,Q,d,qs,fs,num_combs};
+max_comb = max(M(ini+1:end,7)); %Maximo numero de combinaciones
+
+%datos = {m,n,p,Q,d,qs,fs,num_combs};
 
 aks_aux = cellstr(dec2bin(1:(2^p)-1)); %todas las combinaciones de dias posibles
 
 comb = cellstr(dec2bin(M(ini+1:end,8:end),4)); %leer combinaciones y pasarlas a binario
 [~,b]=ismember(comb,aks_aux);  % ver que combinacion de aks le pertence
 b = reshape(b,n,p); %pasar a forma de matriz
-aks = char(aks_aux)';
+aks = (dec2bin(1:(2^p)-1))';
 
+%crear matriz Vk
 
+V = zeros(p,n);
+
+for k=1:p %para cada dia
+    for i=1:n %para cada cliente
+        total = 0;
+        for v=1:max_comb %por cada combinacion
+            if b(i,v) ~= 0
+                val = aks(k,b(i,v))=='1';
+                total = total + val;
+            end
+        end
+        if total >= 1 
+            V(k,i) = 1;
+        end
+    end
+end
+
+%Finalmente: crear matriz I
+
+I = zeros(n,n);
+
+for i=1:n
+    for j=i:n
+        if max(V(:,i)+V(:,j))==2 
+            I(i,j) = 1;
+            I(j,i) = 1;
+        end
+    end
+end   
+        
+    
 end
 
 
