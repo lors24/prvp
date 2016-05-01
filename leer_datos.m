4128144f05ad5cd9f17acfed27ce537fc9e91094
Lore <lors2404@gmail.com>
2016-04-30 21:48:46 -0500

    vehiculos

--- leer_datos.m
+++ leer_datos.m
@@ -4,6 +4,8 @@ m = M(1,2); %numero de vehiculos
 n = M(1,3); %numero de clientes
 p = M(1,4); %numero de dias del periodo
 
+Q = M(2,2); %capacidad, basta con leer uno porque todos los vehiculos tienen la misma capacidad
+
 
 end
 
